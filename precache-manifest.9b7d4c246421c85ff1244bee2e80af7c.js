35e6e6f1fe4830d2af9c5a72d340feac7f9762e0
mpcsj <marcosjunioribm@gmail.com>
2020-03-24 19:26:10 -0300

    Updates

---
+++ precache-manifest.9b7d4c246421c85ff1244bee2e80af7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f395c46d1fe34e097a750774b0feee0b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4519d68d6f0d87c7c69c",
+    "url": "/monsters-rolodex/static/css/main.3620f629.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4519d68d6f0d87c7c69c",
+    "url": "/monsters-rolodex/static/js/main.781d833f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
